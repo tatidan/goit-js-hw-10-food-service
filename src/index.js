8e108aaf8073e51c895e87f45c64c655367ee87a
tatidan <t.danyliuk@gmail.com>
2021-06-29 12:39:46 +0300

    project functions done

--- src/index.js
+++ src/index.js
@@ -1,45 +1,41 @@
-import './styles.css'
+import './styles.css';
+import menu from './menu.json';
+import makeMenu from './template.hbs';
 
+const menuNode = document.querySelector('.js-menu');
+const newMenu = makeMenu(menu);
+menuNode.insertAdjacentHTML('beforeend', newMenu);
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-// Добавь функционал изменения темы при нажатии(событие change)
-// на чекбокс #theme -switch-toggle в тулбаре.
-const bodyRef = document.querySelector('body');
 const checkboxRef = document.querySelector('#theme-switch-toggle');
 checkboxRef.addEventListener('change', onThemeChange);
-
-// По умолчанию тема светлая.
-// При изменении темы, необходимо добавлять на элемент
-// body класс light - theme или dark - theme.
+uploadThemeSelected();
 
 function onThemeChange(event) {
-  if (event.target === Theme.DARK) {
-    bodyRef.classList.add((Theme.DARK));
+  if (event.target.checked) {
+    document.body.classList.toggle(Theme.DARK);
+    localStorage.setItem('dark', true);
   }
-  bodyRef.classList.add((Theme.LIGHT));
- }
+  else {
+    document.body.classList.remove(Theme.DARK);
+    localStorage.setItem('dark', false);
+  }
+}
 
+function uploadThemeSelected(event) {
+  const dataStorage = localStorage.getItem(('dark'));
 
-// Выбранная тема должна сохраняться между
-// перезагрузками страницы.Для хранения темы используй localStorage.
-
-
-// Если при загрузке страницы тема тёмная, не забудь поставить
-// свойство checked у чекбокса #theme -switch-toggle в true,
-// чтобы ползунок сдвинулся в правильное положение.
-
-
-
-// Используя шаблонизатор Handlebars создай шаблон одного элемента меню.
-// После чего, используя шаблон, создай разметку всего меню по данным
-// из menu.json и добавь в DOM в ul.js - menu.
-
-// Для иконок используется библиотека Material Icons, линк на веб - фонт
-// уже есть в исходном HTML.
-
-// Ниже указана разметка элемента меню которая должна получаться
-// по шаблону(данные будут разные для каждого объекта).
+  if (dataStorage === 'true') {
+    document.body.classList.add(Theme.DARK);
+    checkboxRef.checked = true;
+  }
+  else {
+    document.body.classList.remove(Theme.DARK);
+    document.body.classList.add(Theme.LIGHT);
+    checkboxRef.checked = false;
+  }
+}
